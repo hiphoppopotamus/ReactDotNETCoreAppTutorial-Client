f1f87558f25a9b620bdd2182fc79ce10ed910225
Euan Widjaja <EWidjaja@tonkintaylor.co.nz>
2020-11-27 09:58:49 +1300

    Implement pretty much everything stated in tutorial

---
+++ src/components/Home.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import DataTable from './DataTable';
+import RegistrationModal from './form/RegistrationModal';
+import { USERS_API_URL } from '../constants';
+
+class Home extends Component {
+    state = {
+        items: []
+    }
+    componentDidMount() {
+        this.getItems();
+    }
+    getItems = () => {
+        fetch(USERS_API_URL)
+        .then((res) => res.json()) // converts response to json
+        .then((res) => this.setState({ items: res }))
+        .catch(err => console.log(err))
+    }
+    addUserToState = (user) => {
+        this.setState(
+            previous => ({
+                items: [...previous.items, user]
+            })
+        )
+    }
+    updateState = (id) => {
+        this.getItems()
+    } 
+    deleteItemFromState = (id) => {
+        const updated = this.state.items.filter(
+            item => item.id != id
+        )
+        this.setState({ items: updated })
+    }
+    render() {
+        return (
+            <Container style={{ paddingTop: "100px" }}>
+                <Row>
+                    <Col>
+                        <h3>My First React + ASP.NET Core thing I'm pretty sure</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <DataTable items={this.state.items} updateState={this.updateState} deleteItemFromState={this.deleteItemFromState } />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <RegistrationModal isNew={true} addUserToState={this.addUserToState} />
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Home;
